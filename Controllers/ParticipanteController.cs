f4f700b87c23bdbde4e5708f08d5f67ac82eef01
OtavioVB <otaviovb.developer@gmail.com>
2022-07-21 17:10:45 -0300

    fix(base): Estrutura Básica do Projeto

---
+++ Controllers/ParticipanteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIEnem.Controllers
+{
+    [ApiController]
+    [Route("api/v1/content/[controller]")]
+    public class ParticipanteController : ControllerBase
+    {
+        [HttpGet("{NúmeroInscrição}")]
+        public IActionResult Get()
+        {
+            return Ok("Ok");
+        }
+    }
+}
