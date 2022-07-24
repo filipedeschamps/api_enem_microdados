28978c5bad295273a79778abb533085f5952c80d
OtavioVB <otaviovb.developer@gmail.com>
2022-07-24 20:13:25 -0300

    fix(controller): Método `GET` refatorado

--- Controllers/ParticipanteController.cs
+++ Controllers/ParticipanteController.cs
@@ -18,9 +18,25 @@ namespace APIEnem.Controllers
 
 
         [HttpGet("{NúmeroInscrição}")]
-        public string Get(string NúmeroInscrição)
+        public IActionResult Get(string NúmeroInscrição)
         {
-            return _dataParticipante.BUSCAR_INFORMACOES_DO_PARTICIPANTE(new NúmeroInscrição(NúmeroInscrição)).ToString();
+            try
+            {
+                string Retorno = _dataParticipante.BUSCAR_INFORMACOES_DO_PARTICIPANTE(new NúmeroInscrição(NúmeroInscrição)).ToString();
+
+                if (Retorno is null)
+                {
+                    return NotFound("Não foi possível encontrar o número de inscrição");
+                }
+                else
+                {
+                    return Ok(Retorno);
+                }
+            }
+            catch
+            {
+                return BadRequest("Erro interno da API");
+            }
         }
     }
 }
